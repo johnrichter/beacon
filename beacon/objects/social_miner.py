c80ce967f76e3407c2446ac0f48b7db67cf867e6
John Richter <john@johnathanrichter.com>
2015-09-28 21:23:56 -0400

    Researching AngelList API capabilities

--- beacon/objects/social_miner.py
+++ beacon/objects/social_miner.py
@@ -11,6 +11,19 @@ class SocialMiner(object):
         limitation. Other notable information we can gather is the user's name, profile picture,
         and banner picture.
 
+    .. note::
+
+        AngelList allows lookup by URL slug (i.e. the link text a user has chosen for their profile,
+        e.g. James Bond could be using slug: james-bond) or MD5 hash of a user's email address.
+        Other notable information we can gather is blog_url, online_bio_url, twitter_url,
+        facebook_url, linkedin_url, angellist_url, dribble_url, github_url, resume_url,
+        profile picture, and full name.  We may be able to parse the bio, what_i_do,
+        and what_ive_built fields for email and username information.
+
+    .. note
+
+        LinkedIn
+
     .. _any API endpoint: https://dev.twitter.com/faq#26
     """
     def __init__(self):
